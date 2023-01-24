e28a05537841b4bf3fd883d43720541ff03eed3c
zhenechkaaaa <94906626+zhenechkaaaa@users.noreply.github.com>
2023-01-24 16:26:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-if __name__ == '__main__':
-    main()
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='127.0.0.1', port=port)
+#if __name__ == '__main__':
+    #main()
+    #port = int(os.environ.get("PORT", 8080))
+    #app.run(host='127.0.0.1', port=port)
